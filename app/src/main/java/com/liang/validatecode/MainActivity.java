69e3335a10d42ff88dbe504c44decfe0bc3b6151
liangyang <1935990239@qq.com>
2017-02-06 18:00:43 +0800

    ValidateCode短信验证码自动填写功能

--- app/src/main/java/com/liang/validatecode/MainActivity.java
+++ app/src/main/java/com/liang/validatecode/MainActivity.java
@@ -1,21 +1,47 @@
 package com.liang.validatecode;
 
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
+/**
+ * 短信验证码自动监听填写
+ */
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MSG_RECEIVED_CODE = 1;
+
     private SmsObserver smsObserver;
+
+    /**
+     * 使用Handler在非UI中更新UI(是一个非常常用的方法)
+     */
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_RECEIVED_CODE){
+                String code = (String) msg.obj;
+                //更新UI
+                mValidateCode.setText(code);
+            }
+
+        }
+    };
+    private EditText mValidateCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        smsObserver = new SmsObserver(MainActivity.this,null);
+        mValidateCode = (EditText) findViewById(R.id.et_validate_code);
+
+        smsObserver = new SmsObserver(MainActivity.this, mHandler);
         Uri uri = Uri.parse("content://sms");
-        getContentResolver().registerContentObserver(uri,true,smsObserver);
+        getContentResolver().registerContentObserver(uri, true, smsObserver);
     }
 
     @Override
--- app/src/main/java/com/liang/validatecode/SmsObserver.java
+++ app/src/main/java/com/liang/validatecode/SmsObserver.java
@@ -2,9 +2,13 @@ package com.liang.validatecode;
 
 import android.content.Context;
 import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by dell on 2017/2/5.
@@ -15,9 +19,9 @@ public class SmsObserver extends ContentObserver {
     private Context context;
     private Handler handler;
 
-    public SmsObserver(Context mContext,Handler handler) {
+    public SmsObserver(Context context, Handler handler) {
         super(handler);
-        this.context = mContext;
+        this.context = context;
         this.handler = handler;
     }
 
@@ -28,8 +32,35 @@ public class SmsObserver extends ContentObserver {
         Log.e(TAG, "SMS has changed!");
         Log.e(TAG, "onChange: " + uri.toString());
 
-        if (uri.toString().equals("content://sms/raw")){
+        String code = "";
+
+        if (uri.toString().equals("content://sms/raw")) {
             return;
+        }
+
+        Uri inboxUri = Uri.parse("content://sms/inbox");
+        Cursor cursor = context.getContentResolver().query(inboxUri, null, null, null, "date desc");
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                String address = cursor.getString(cursor.getColumnIndex("address"));
+                String body = cursor.getString(cursor.getColumnIndex("body"));
+
+                Log.e(TAG, "发件人为： " + address + " " + "短信内容为：" + body);
+
+                //正则表达式---提取短信中连续6个数字的内容
+                Pattern pattern = Pattern.compile("(\\d{6})");
+                Matcher matcher = pattern.matcher(body);
+
+                if (matcher.find()) {
+                    code = matcher.group(0);
+
+                    Log.e(TAG, "code is : " + code);
+
+                    //向Handler发送消息
+                    handler.obtainMessage(MainActivity.MSG_RECEIVED_CODE,code).sendToTarget();
+                }
+            }
+            cursor.close();
         }
     }
 }
